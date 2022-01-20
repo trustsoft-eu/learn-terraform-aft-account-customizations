816dbfee958a46645086e98c365e98e89cd77c47
Stefan Matic <stefan.matic@trustsoft.eu>
2022-01-20 16:07:19 +0100

    Add sandbox bucket using trustsoft module

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,3 +4,9 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+module "s3-trustsoft-org" {
+    source      = "git::https://gitlab.lukapo.com/terraform/aws/module.s3?ref=1.0.2"
+    bucket_name = "created-from-aft-customizations-1231231245"
+    user_name   = "created-from-aft-customizations-1231231245"
+}
